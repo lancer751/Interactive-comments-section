44d83904f5104cdda9dc5becd7a412077572f26c
lanc3YT <pachecolau27@gmail.com>
2024-03-29 16:35:23 -0500

    se hicieron los componentes card-comment y card-response.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      "Moderate-blue": "hsl(238, 40%, 52%)",
+      "Soft-Red": "hsl(358, 79%, 66%)",
+      "Light-grayish-blue": "hsl(239, 57%, 85%)",
+      "Pale-red": "hsl(357, 100%, 86%)",
+      "Dark-blue": "hsl(212, 24%, 26%)",
+      "Grayish-Blue": "hsl(211, 10%, 45%)",
+      "Light-gray": "hsl(223, 19%, 93%)",
+      "Very-light-gray": "hsl(228, 33%, 97%)",
+      "white": "hsl(0, 0%, 100%)",
+    },
+    fontFamily:{
+      "Rubik": ["Rubik", 'sans-serif'],
+    },
+    extend: {
+      boxShadow:{
+        '2xl': '5px 0px 2px -3px hsl(223, 19%, 93%)',
+      }
+    },
   },
   plugins: [],
 }
